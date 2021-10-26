72b2521cec7bb75e24007acfb8d2d5642e610953
Nikita Teterin <nekitonum@gmail.com>
2021-10-26 05:18:45 +0300

    add comments

--- lab2/permutations.cpp
+++ lab2/permutations.cpp
@@ -3,6 +3,8 @@
 using namespace utils;
 
 static void swap(size_t *a, size_t *b){
+    // swap provided items, quite self-explanatory
+    // just check whether they point to the same adress and pass if so
     if (a == nullptr || b == nullptr) return;
     if (a == b) return;
     size_t tmp = *a;
@@ -26,6 +28,11 @@ PermutationEnumerator::~PermutationEnumerator(){
 }
 
 int PermutationEnumerator::init_arrays(const size_t n){
+    /*
+    inits arrays with accordance to the algorithm
+    permutations: indices are values (except bounds, these are n + 1)
+    directions: all -1 (left), once again except bounds and also 1 (these are 0, i.e. static)
+    */
     directions = new int[n+2];
     permutation = new size_t[n+2];
     reverse_permutation = new size_t[n+2];
@@ -40,11 +47,17 @@ int PermutationEnumerator::init_arrays(const size_t n){
 }
 
 const size_t PermutationEnumerator::get_neighbour(const size_t j){
+    /*
+    utilize the reverse permutation property:
+    its value at specified index is index of this
+    element in the original array (also vice versa, though not used)
+    */
     size_t i = reverse_permutation[j] + directions[j];
     return permutation[i];
 }
 
 const size_t PermutationEnumerator::get_current_m(){
+    // current m is the largest item which is greater than its 'signed' neighbour
     for (size_t i = n; i > 1; --i){
         if ( i > get_neighbour(i) ) return i;
     }
@@ -52,13 +65,15 @@ const size_t PermutationEnumerator::get_current_m(){
 }
 
 int PermutationEnumerator::transpose(const size_t i, const size_t j){
+    // transposition = swap
     swap(&permutation[ reverse_permutation[j] ], &permutation[ reverse_permutation[i] ]);
     swap(&reverse_permutation[j], &reverse_permutation[i]);
     return EXIT_SUCCESS;
 }
 
 int PermutationEnumerator::update_directions(const size_t m){
-
+    // important step is reversing directions of all items numerically greater then current m
+    // e.g., if there are 7 items and current m is 5, directions of 6 and 7 should be updated
     auto change_direction = [](int & direction){
         direction *= CHANGE_SIGN;
     };
@@ -70,7 +85,7 @@ int PermutationEnumerator::update_directions(const size_t m){
 }
 
 void PermutationEnumerator::traverse(){
-
+    // it merely does what its name says
     print(std::cout, 1);
     for (size_t m = get_current_m(), i = 2; m != 1; m = get_current_m(), ++i){
         transpose(m, get_neighbour(m));
@@ -81,6 +96,9 @@ void PermutationEnumerator::traverse(){
 }
 
 void PermutationEnumerator::print(std::ostream &out, const size_t n_iter){
+    // pretty-print current state of permutations
+    // the cringe here is my design where stream is provided to this method but not provided to `traverse()`
+    // whatever this is not what matters
     
     auto equal = [&]() {
         for (size_t i = 1; i < n + 1; ++i){
--- lab2/permutations.hpp
+++ lab2/permutations.hpp
@@ -15,6 +15,29 @@ namespace utils{
         BAD_ITEMS = -1
     };
 };
+/*
+    utils namespace contains enum for codes used later in code
+    and the very enumerator class
+    latter wraps 3 arrays and implements the transposition algorithm
+
+    the attributes are quite self-explanatory:
+    + directions: array of encoded directions of corresponding elements,
+        -1 stands for left, 1 for right, 0 is assigned to 0 and fake bounding elements
+    + permutation: array containing the current permutation. size is n + 2 (n plus two fake items)
+    + reverse permutation: array containing the current reverse permutation. same size with others,
+        indices of this array correspond to values of the previous one while values correspond to
+        indices of ordinary permutations array and vice versa
+    + n: the number of items (natural number)
+
+    I explicitly delete the default init constructor
+    in order to prohibit user creating enumerator on empty set
+    the constructor will also throw exception if the n given is 0
+
+    traverse() method is the interface of enumerator, 
+    it iterates over permutations and prints current one
+
+    comments on other methods are in the source file
+*/
 
 class utils::PermutationEnumerator{
 private:
@@ -29,13 +52,13 @@ private:
     int init_arrays(const size_t);
     int transpose(const size_t i, const size_t j);
     int update_directions(const size_t);
+    void print(std::ostream & out, const size_t iter);
 
 public:
     PermutationEnumerator() = delete;
     PermutationEnumerator(const size_t);
     ~PermutationEnumerator();
     void traverse();
-    void print(std::ostream & out, const size_t iter);
 };
 
 #endif // _PERMUTATIONS_HPP
